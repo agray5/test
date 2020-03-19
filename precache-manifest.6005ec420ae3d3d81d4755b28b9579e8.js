7d3f955751dc0b7420b632a86b7899ddc85478ef
April Gray <grayaprild@gmail.com>
2020-03-19 00:20:44 -0500

    update

---
+++ precache-manifest.6005ec420ae3d3d81d4755b28b9579e8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0abfc05cc092176094370a5ac505e86b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "080f6b88966b122d58a2",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4f70476c93ad483d121e",
+    "url": "./static/js/2.7277773e.chunk.js"
+  },
+  {
+    "revision": "df3251ffffe613334ac836354ea3a310",
+    "url": "./static/js/2.7277773e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "080f6b88966b122d58a2",
+    "url": "./static/js/main.a31e67f6.chunk.js"
+  },
+  {
+    "revision": "8f22ef2a2fa63c557300",
+    "url": "./static/js/runtime-main.bc1153a5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/dashboard/precache-manifest.a9f56130441ff4d674ceb1853812a576.js"
+  "./precache-manifest.6005ec420ae3d3d81d4755b28b9579e8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/dashboard/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
